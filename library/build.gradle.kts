0a4a7118495f27b329c39c6e41d98bfd5beb5e4d
Cosmin Mihu <cosmin.mihu@gmail.com>
2025-04-07 18:08:27 +0300

    Bump library version to 0.0.3

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -14,7 +14,7 @@ kotlin {
 val module = "toast"
 val artifact = "toast"
 group = "ro.cosminmihu.toast"
-version = "0.0.2"
+version = "0.0.3"
 
 mavenPublishing {
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
