7372b3d7b05503cd7e4ba30aad1ea1c7170b0650
Cosmin Mihu <cosmin.mihu@gmail.com>
2025-04-07 17:56:05 +0300

    Update library version to 0.0.2

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -14,7 +14,7 @@ kotlin {
 val module = "toast"
 val artifact = "toast"
 group = "ro.cosminmihu.toast"
-version = "0.0.1"
+version = "0.0.2"
 
 mavenPublishing {
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
